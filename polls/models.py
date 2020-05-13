f8bc6ac69ec2d9f3b60881468b9b92caa066751e
matheuspp <matheusparaiso98@gmail.com>
2020-05-13 01:26:33 -0300

    fixed a bug

--- polls/models.py
+++ polls/models.py
@@ -8,7 +8,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published(self):
-        return self.pub_date >= timezone.now()
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 datetime.timedelta(days=1)
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
